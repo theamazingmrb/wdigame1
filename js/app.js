be8c0cf6569f68a9e59642df557721335650eec0
billie heidelberg <billie.heidelberg.jr@gmail.com>
2017-02-06 15:34:39 -0800

    adding difficulty choice

--- js/app.js
+++ js/app.js
@@ -1,30 +1,84 @@
+
+/////////////////////////////////////////// start the game
 function startGame(){
+  $('<img src="imgs/PressStart.jpg">').appendTo(".gameBoard"); 
 
 
-  setMessage("It works");
+  setMessage("Welcome to You Guess It!");
+  setQuestion(" Please choose Start Game!")
+  $('.difficultyboxH').remove();
+  $('.difficultyboxM').remove();
+  $('.difficultyboxE').remove();
+
+
+
 }
 startGame();
 
+/////////////////////////////// creates set question needed functions
+function setQuestion(ques){
+ $('#question').text(ques);
+}
+///////////////////////////// creates chooseDifficulty
+function chooseDifficulty(){
+  setMessage("Please choose your difficulty")
+
+  setQuestion("Easy, " + " Medium," + " Hard") // this works to change the text
+  var hard = $('<div class="difficultyboxH"></div>').css("background-color","white").text("Hard")
+  $('.difficultyBoxH').append(hard);
+
+
+var medium = $('<div class="difficultyboxM"></div>').css("background-color","orange").text("Medium")
+$('.difficultyBoxM').append(medium);
+
+var easy = $('<div class="difficultyboxE"></div>').css("background-color","blue").text("Easy")
+$('.difficultyBoxE').append(easy);
+
+
+
+}
+
+//////////////////////////////// difficuty functions
+$('.difficultyBoxE').click(function setEasyQuestions(){
+
+
+  setMessage("Easy it is");
+  $('gameBoard').
+  $('.difficultyboxH').remove();
+  $('.difficultyboxM').remove();
+  $('.difficultyboxE').remove();
+  $('#question').text(sportsQuestions);
+})
+
+
+
+
+//////////////////////// sets message function
 
 
 function setMessage(msg){
-  console.log("hit setmessage")
+
   $('#message').text(msg);
 }
 
-
+//////////////////// creates hoover over buttons
 $('.button').hover(function(){
   $(this).fadeOut(500).slideDown(500)
 });
+
+
+//////////////////////////creats click for both buttons
 $('.button:first').click(function(){
-  alert("you clicked New Game")
+  startGame();
+  $('#bigBoard').css("background-color", "black")
+  chooseDifficulty();
+  $()
+
 });
 $('.button:last').click(function(){
-  alert("you clicked Rematch")
+   startGame();
 });
-$('html').onload = function(){
-  console.log("onload worked")
-}
+//////////////////////////////////////////////////// Game LOGIX
 
 var sportsQuestions = [
     ["sport question 1?", "a", "b", "c", "d", "answer"],
